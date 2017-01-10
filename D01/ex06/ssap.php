60089e956d3860a101fa3895bf9eaf99784cfa1f
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-10 23:49:14 +0100

    ssap finished

--- D01/ex06/ssap.php
+++ D01/ex06/ssap.php
@@ -6,20 +6,49 @@
 		$str = trim($str);
 		$tab = explode(' ', $str);
 		$filter = array_filter($tab);
-		return ($str);
+		return ($filter);
 	}
+
+	function array_shift_tab($tab)
+	{
+		$ret = array();
+		for ($i=0; $i < count($tab); $i++) { 
+			$ret[] = array_shift($tab[$i]);
+		}
+		return $ret;
+	}
+
 	if ($argc < 2) {
 		exit();
 	}
-	$tab = array_merge_recursive($argv);
-	unset($tab[0]);
-
-	foreach ($tab as $key => $value) {
-		var_dump(str_word_count($value, 1));
-		
+	$tab = array();
+	for ($i = 1; $i < count($argv); $i++) { 
+		$tab[] = ft_split($argv[$i]);
 	}
-
-	//var_dump($tab);
-
+	$tab_value = array();
+	for ($i = 0; $i < count($tab); $i++) {
+		if (count($tab[$i]) > 1) {
+			for ($j = 0; $j < count($tab[$i]); $j++) { 
+				$tab_value[] = ft_split($tab[$i][$j]);
+			}
+		}
+		else
+			$tab[$i] = array_shift($tab[$i]);
+	}
+	$ret = array_merge($tab, array_shift_tab($tab_value));
+	for ($i = 0; $i < count($ret); $i++) { 
+		if (is_array($ret[$i]))
+		{
+			$ret[$i] = array_splice($ret[$i], $i, -1);
+			unset($ret[$i]);
+		}
+	}
+	sort($ret);
+	for ($i = 0; $i < count($ret); $i++) {
+		if ((count($ret) - 1) == $i)
+			echo $ret[$i];
+		else
+			echo $ret[$i]."\n";
+	}
 
 ?>
