de76e32f7a5be2d05426efd78550fbd5cf1db639
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-11 11:24:50 +0100

    ok cookie

--- D03/ex03/cookie_crisp.php
+++ D03/ex03/cookie_crisp.php
@@ -3,19 +3,32 @@
 	if (isset($_GET) && !empty($_GET))
 	{
 
-		if isset($_GET['get']) && !empty($_GET['get']) {
-
-		}
-		if isset($_GET['set']) && !empty($_GET['set']) {
-			
-		}
-		if isset($_GET['del']) && !empty($_GET['del']) {
-			
-		}
-		if isset($_GET['get']) && !empty($_GET['get']) {
-			
+		foreach ($_GET as $key => $value) {
+			if ($key == 'action'){
+				$action = $value;
+			} else if ($key == 'name') {
+				$name = $value;
+			} else if ($key == 'value') {
+				$val = $value;
+			}
 		}
 
+		if ($action == "get")
+		{
+			if (!empty($name) && $_COOKIE[$name])
+				echo $_COOKIE[$name]."\n";
+		}
+		else if ($action == "set")
+		{
+			if (!empty($name) && !empty($val))
+				setcookie ($name, $val, time() - 3600,  "cookie.txt");
+		}
+		else if ($action == "del")
+		{
+			if (!empty($name))
+				setcookie ($name, "", time() - 3600,  "cookie.txt");
+
+		}
 	}
 
 ?>
