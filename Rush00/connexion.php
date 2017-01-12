5047ed9a8d9cb14b024c8af9cb70f81123b6c3db
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-12 14:34:27 +0100

    ok

--- Rush00/connexion.php
+++ Rush00/connexion.php
@@ -1,12 +1,5 @@
-<?php include "./layout/header.php"; ?>
+<?php 
 
-	<div class="content">
-		<h1>Se connecter</h1>	
-		<form action="connexion.php" method="post">
-			<input type="text" name="login">
-			<input type="password" name="password">
-			<button type="submit">Se connecter</button>
-		</form>
-	</div>
-	<?php include "./layout/aside.php"; ?>
-<?php include "./layout/footer.php"; ?>		
+	header("Location: index.php");
+
+?>	
