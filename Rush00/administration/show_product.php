a86ff1db55f1bb7143e7010c5535f78056b572ff
Wasim GULISTAN <wgulista@e2r2p22.42.fr>
2017-01-15 22:14:27 +0100

    ok

--- Rush00/administration/show_product.php
+++ Rush00/administration/show_product.php
@@ -20,13 +20,14 @@
 					<th>Delete</th>
 				</tr>
 				<?php 
-				$resultat = mysqli_query($bdd, "SELECT * FROM products p LEFT JOIN categories c ON (p.category_id = c.id);");
-				while ($d = mysqli_fetch_assoc($resultat)) { var_dump($d);?>
+				$sql = "SELECT p.id, p.name, p.content, p.price, p.category_id, c.name as cat_name FROM products p LEFT JOIN categories c ON (p.category_id = c.id);";
+				$resultat = mysqli_query($bdd, $sql);
+				while ($d = mysqli_fetch_assoc($resultat)) {?>
 					<tr>
 						<td><?php echo $d['id']; ?></td>
 						<td><?php echo $d['name']; ?></td>
 						<td><?php echo $d['content']; ?></td>
-						<td><?php echo $d['category_id']; ?></td>
+						<td><?php echo $d['cat_name']; ?></td>
 						<td><a href="update_product.php?id=<?php echo $d['id']; ?>"><img src="http://www.freeiconspng.com/uploads/blue-refresh-icon-png-6.png" height="20" salt=""></a></td>
 						<td><a href="delete_product.php?id=<?php echo $d['id']; ?>"><img src="http://findicons.com/files/icons/1262/amora/128/delete.png" height="20" alt=""></a></td>
 					</tr>
