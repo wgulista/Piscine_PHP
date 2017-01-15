ff72f427a05e0f0b641366246e9bb3b456fdddca
Victorien CAQUANT <vcaquant@e2r2p23.42.fr>
2017-01-15 23:06:57 +0100

    auto commit

--- Rush00/info_product.php
+++ Rush00/info_product.php
@@ -1,17 +1,22 @@
 <?php include "./layout/header.php"; ?>
 
+<?php $req = mysqli_query($bdd, 'SELECT * FROM products WHERE id="'.$_GET[id].'"'); ?>
+<?php $row = mysqli_fetch_assoc($req); ?>
 	<div class="content">
-		
+
 		<div class="info_product">
-			<img src="http://static.aujardin.info/cache/th/img9/rosa-fleur-600x450.jpg" width="100%" alt="">
+			<img src="<?php echo $row[image] ?>" width="100%" alt="">
 			<h2>Titre</h2>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur ipsam illo maiores amet labore, expedita laborum, architecto officiis esse veniam sunt eum dolores ducimus eaque error alias cum. Eligendi, excepturi?
+				<?php echo $row[content] ?>
 			</p>
 			<h3>prix</h3>
-			<a class="add_cart" href="">Ajouter au panier</a>
+			<p>
+				<?php echo $row[price] ?> &euro;
+			</p>
+			<a class="add_cart" href="panier.php?add=<?php echo $row['id']; ?>">Ajouter au panier</a>
 		</div>
 
 	</div>
 	<?php include "./layout/aside.php"; ?>
-<?php include "./layout/footer.php"; ?>		
+<?php include "./layout/footer.php"; ?>
