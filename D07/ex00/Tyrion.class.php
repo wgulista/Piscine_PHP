f78469bf31abb0e8623f425310c4864c2dd5f631
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-16 11:32:25 +0100

    ok

---
+++ D07/ex00/Tyrion.class.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class Tyrion extends Lannister
+	{
+		
+		function __construct()
+		{
+			parent::__construct();
+			print("My name is Tyrion" . PHP_EOL); 
+		}
+
+		public function getSize() {
+			return "Short";
+		}
+
+	}
+
+
+?>
