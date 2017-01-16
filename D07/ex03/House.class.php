e9652589bcb249d4a3751520175215e0c29240f5
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-16 11:47:24 +0100

    ok ex03

---
+++ D07/ex03/House.class.php
@@ -0,0 +1,14 @@
+<?php 
+
+	class House
+	{
+		
+		public function introduce() {
+			print "House ". $this->getHouseName()
+						." of ".$this->getHouseSeat()
+						." : \"". $this->getHouseMotto() . "\"\n";
+		}	
+
+	}
+
+?>
