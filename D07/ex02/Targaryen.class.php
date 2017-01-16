d13a37d25287a70cc4bc4ba04874e83e12b1c397
Gulistan Wasim <git.wasim.gulistan@gmail.com>
2017-01-16 11:41:56 +0100

    exo2 ok

---
+++ D07/ex02/Targaryen.class.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class Targaryen
+	{
+		
+		public function resistsFire() {
+			return false;
+		}
+
+		public function getBurned(){
+			if (!$this->resistsFire()) {
+				return "burns alive";
+			} else {
+				return "emerges naked but unharmed";
+			}
+		}
+	}
+
+?>
